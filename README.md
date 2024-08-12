8783d02d25baf0bc8de7b15d45d61acef0c775d9
cchristion <47944263+cchristion@users.noreply.github.com>
2024-08-12 17:57:51 +0000

    Major Revamp

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # tree_parser
 
-This document provides instructions for processing files generated from the `tree` command and converting them to relative paths.
+This Python script converts the output of the `tree` command into a list of relative file paths.
 
 ## Instructions
 
 - **Run the Python script:**
    ```sh
-   python tree_parser.py input.txt output.txt
+   python tree_parser.py input.txt > output.txt
    ```
--- tree_parser.py
+++ tree_parser.py
@@ -3,49 +3,41 @@ import re
 import sys
 from pathlib import Path
 
-if len(sys.argv) < 3:
-    print("command:\npython tree_parser.py input.txt output.txt")
+if len(sys.argv) < 2:
+    print("command:\npython tree_parser.py input.txt > output.txt")
     sys.exit()
 
 
 file_map = {}
 path_list = []
-p1 = re.compile(r"\x1B\[[\d;]+m")
-p2 = re.compile(r"[└├│]+")
-p3 = re.compile(r"── ")
-p4 = re.compile("\xa0\xa0 ")
-p5 = re.compile("\xa0\xa0     ")
 
-print("Processing...")
+p1 = re.compile(r"\x1B\[[\d;]+m")
+p2 = re.compile(r"^.*── ")
+p3 = re.compile("\xa0\xa0 ")
+p4 = re.compile(r"\s{4}")
 
 with Path.open(sys.argv[1], "r") as fr:
-    with Path.open(sys.argv[2], "w") as fw:
-        for line in fr:
+    for line in fr:
 
-            if not p3.search(line):
-                continue
+        line_s = p2.search(line)
+        if not line_s:
+            continue
 
-            line = p1.sub("", line)
-            line = p2.sub("", line)
-            line = p3.sub("", line)
-            depth = len(p4.findall(line))
-            depth += len(p5.findall(line))
-            line = p4.sub("", line)
-            line = p5.sub("", line)
+        line = p1.sub("", line)
+        line = p2.sub("", line)
+        depth = len(p3.findall(line_s.group(0)))
+        depth += len(p4.findall(line_s.group(0)))
 
-            if depth in file_map:
-                path = Path()
-                for entry in file_map:
-                    path = path / file_map[entry]
+        if depth in file_map:
+            path = Path()
+            for entry in file_map:
+                path = path / file_map[entry]
 
-                fw.write(repr(str(path)))
-                fw.write("\n")
-                file_map_t = copy.deepcopy(file_map)
+            print(repr(str(path)))
+            file_map_t = copy.deepcopy(file_map)
 
-                for entry in file_map_t:
-                    if entry > depth:
-                        del file_map[entry]
+            for entry in file_map_t:
+                if entry > depth:
+                    del file_map[entry]
 
-            file_map[depth] = line.strip("\xa0" + " \n")
-
-print("Done.")
+        file_map[depth] = line.strip("\xa0" + " \n")
