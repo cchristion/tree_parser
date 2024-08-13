0b363e87a0bf0dfe8b6b131459d6deca707f5aba
cchristion <47944263+cchristion@users.noreply.github.com>
2024-08-13 10:21:56 +0000

    Fixed - Path.open issue in python3.8 - Last line not parsing

--- tree_parser.py
+++ tree_parser.py
@@ -8,6 +8,13 @@ if len(sys.argv) < 2:
     sys.exit()
 
 
+def path_print(file_map):
+    path = Path()
+    for entry in file_map:
+        path = path / file_map[entry]
+    print(repr(str(path)))
+
+
 file_map = {}
 path_list = []
 
@@ -16,7 +23,8 @@ p2 = re.compile(r"^.*── ")
 p3 = re.compile("\xa0\xa0 ")
 p4 = re.compile(r"\s{4}")
 
-with Path.open(sys.argv[1], "r") as fr:
+file = Path(sys.argv[1])
+with file.open() as fr:
     for line in fr:
 
         line_s = p2.search(line)
@@ -29,11 +37,7 @@ with Path.open(sys.argv[1], "r") as fr:
         depth += len(p4.findall(line_s.group(0)))
 
         if depth in file_map:
-            path = Path()
-            for entry in file_map:
-                path = path / file_map[entry]
-
-            print(repr(str(path)))
+            path_print(file_map)
             file_map_t = copy.deepcopy(file_map)
 
             for entry in file_map_t:
@@ -41,3 +45,5 @@ with Path.open(sys.argv[1], "r") as fr:
                     del file_map[entry]
 
         file_map[depth] = line.strip("\xa0" + " \n")
+
+path_print(file_map)
